0a32042a440081c5dee527a4c5815c0009116909
Rainbow <Rainbow@SURFACEPRO6>
2020-11-08 15:00:33 +0800

    增加管理员查看和编辑分数功能 增加评分细则备注字段

--- Competition.Web/Scripts/MyFunction.js
+++ Competition.Web/Scripts/MyFunction.js
@@ -51,7 +51,7 @@ function watermark(text) {
         watermark_color: '#aaa',
         //watermark_color: '#848484',//水印字体颜色
         watermark_alpha: 0.4,//水印透明度
-        watermark_fontsize: '100px',//水印字体大小
+        watermark_fontsize: '80px',//水印字体大小
         watermark_font: '微软雅黑',//水印字体
         watermark_width: 300,//水印宽度
         watermark_height: 300,//水印长度
@@ -112,3 +112,5 @@ function watermark(text) {
     };
     document.body.appendChild(oTemp);
 }
+
+
